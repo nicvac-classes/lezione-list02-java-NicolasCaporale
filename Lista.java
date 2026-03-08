dd67a21702ea10a146fb786a9be528d88f51eec5
NicolasCaporale <nicolas.caporale@marconibari.edu.it>
2026-03-08 22:24:52 +0000

    dgjisehgudfb

--- Lista.java
+++ Lista.java
@@ -1,4 +1,8 @@
+
+import java.util.NoSuchElementException;
+
 public class Lista<T> {
+
     private Nodo<T> head;
 
     public Nodo<T> getHead() {
@@ -14,10 +18,7 @@ public class Lista<T> {
     }
 
     public boolean isEmpty() {
-        if (head == null) {
-            return true;
-        }
-        return false;
+        return head == null;
     }
 
     public void aggiungiTesta(T valore) {
@@ -32,39 +33,170 @@ public class Lista<T> {
             head = nuovoNodo;
             return;
         }
-        Nodo<T> corrente = head;
-        while (corrente.getNext() != null) {
-            corrente = corrente.getNext();
+        Nodo<T> curr = head;
+        while (curr.getNext() != null) {
+            curr = curr.getNext();
         }
-        corrente.setNext(nuovoNodo);
+        curr.setNext(nuovoNodo);
     }
 
-    public void aggiungiPosizione(T valore, int posizione) throws IndexOutOfBoundsException {
-        if (posizione == 0) {
+    public void aggiungipos(T valore, int pos) {
+        if (pos == 0) {
             aggiungiCoda(valore);
-        } else {
-
-            Nodo<T> corrente = head;
-            int tette = 0;
-            while (corrente.getNext() != null && tette < posizione - 1) {
-                corrente = corrente.getNext();
-                ++tette;
-            }
-
-            if (tette < posizione) {
-                throw new IndexOutOfBoundsException("Posizione non valida");
-            }
-            Nodo<T> nuovoNodo = new Nodo<>(valore);
-            nuovoNodo.setNext(corrente.getNext());
-            corrente.setNext(nuovoNodo);
+            return;
         }
+
+        if (pos < 0) {
+            throw new IndexOutOfBoundsException("pos sotto zero");
+        }
+
+        Nodo<T> curr = head;
+        int contPos = 0;
+        while (curr != null && contPos < pos - 1) {
+            curr = curr.getNext();
+            ++contPos;
+        }
+
+        if (curr == null) {
+            throw new IndexOutOfBoundsException("pos non valida");
+        }
+        Nodo<T> nuovoNodo = new Nodo<>(valore);
+        nuovoNodo.setNext(curr.getNext());
+        curr.setNext(nuovoNodo);
+
     }
 
     public T leggiTesta() {
         if (head == null) {
             throw new NoSuchElementException("Lista vuota");
         }
-        return head.dato;
+        return head.getValore();
     }
 
+    public T leggiCoda() {
+        if (head == null) {
+            throw new NoSuchElementException("Lista vuota");
+        }
+
+        Nodo<T> curr = head;
+
+        while (curr.getNext() != null) {
+            curr.setNext(curr.getNext());
+        }
+
+        return curr.getValore();
+    }
+
+    public T leggiInpos(int pos) {
+        if (pos < 0) {
+            throw new IndexOutOfBoundsException("pos negativa");
+        }
+
+        Nodo<T> curr = head;
+        int i = 0;
+
+        while (curr != null && i < pos) {
+            curr = curr.getNext();
+            i++;
+        }
+
+        if (curr == null) {
+            throw new IndexOutOfBoundsException("pos oltre la fine della lista");
+        }
+
+        return curr.getValore();
+    }
+
+    public int size() {
+        int cont = 0;
+        Nodo<T> curr = head;
+
+        while (curr != null) {
+            ++cont;
+            curr = curr.getNext();
+        }
+
+        return cont;
+    }
+
+    public int indexOf(int pos) {
+        if (pos < 0) {
+            throw new IndexOutOfBoundsException("pos negativa");
+        }
+        Nodo<T> curr = head;
+        int cont = 0;
+        while (curr != null && cont < pos) {
+            ++cont;
+            curr = curr.getNext();
+        }
+
+        if (curr == null) {
+            throw new IndexOutOfBoundsException("pos non valida");
+        }
+        return cont;
+    }
+
+    public boolean contiene(int elemento) {
+        Nodo<T> curr = head;
+        while (curr != null) {
+            if (curr.getValore().equals(elemento)) {
+                return true;
+            }
+            curr = curr.getNext();
+        }
+        return false;
+    }
+
+    public boolean cancella(T dato) {
+        if (head == null) {
+            return false;
+        }
+
+        if (head.getValore().equals(dato)) {
+            head = head.getNext();
+            return true;
+        }
+
+        Nodo<T> precedente = head;
+        Nodo<T> curr = head.getNext();
+
+        while (curr != null) {
+            if (curr.getValore().equals(dato)) {
+                precedente.setNext(curr.getNext());
+                return true;
+            }
+            precedente = curr;
+            curr = curr.getNext();
+        }
+
+        return false;
+    }
+
+    public T cancellaInpos(int pos) {
+        if (pos < 0 || head == null) {
+            throw new IndexOutOfBoundsException("pos non valida");
+        }
+
+        if (pos == 0) {
+            T dato = head.getValore();
+            head = head.getNext();
+            return dato;
+        }
+
+        Nodo<T> precedente = head;
+        int i = 0;
+
+        while (precedente.getNext() != null && i < pos - 1) {
+            precedente = precedente.getNext();
+            i++;
+        }
+
+        if (precedente.getNext() == null) {
+            throw new IndexOutOfBoundsException("pos oltre la fine della lista");
+        }
+
+        T dato = precedente.getNext().getValore();
+        precedente.setNext(precedente.getNext().getNext());
+        return dato;
+    }
 }
